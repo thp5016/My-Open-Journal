41c8f6c2804171007bca69878e2eb18578379455
Tyler Purdom <thp5016@psu.edu>
2013-04-10 16:15:48 -0400

    added cookies to login

---
+++ My Open Journal/src/Test/Login.java
@@ -0,0 +1,44 @@
+package Test;
+
+import java.sql.Statement;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zul.Longbox;
+import org.zkoss.zul.Textbox;
+
+public class Login extends SelectorComposer<Component>{
+	
+	@Wire 
+	Textbox username;
+	
+	@Wire
+	Textbox pwd;
+	
+    @Listen("onClick = #login")
+    public void LoginUser(){
+    	DBManager manager = new DBManager();
+    	if(manager.IsValidUser(username.getText()))
+    	{
+    		if(manager.IsValidPassword(username.getText(), pwd.getText()))
+    		{
+                CookieManager.setCookie(username.getText(), pwd.getText());
+    			Executions.sendRedirect("index.zul");
+    		}
+    		else
+    			System.out.println("Incorrect Password!!");
+    	}
+    	else
+    		System.out.println("Invalid User!!");
+    }
+    
+    @Listen("onClick = #register")
+    public void RegisterUser() {
+    	Executions.sendRedirect("register.zul");
+    }
+
+}
