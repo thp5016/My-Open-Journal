244e6ddfd35941e3e64a218c863aba35c9cfe903
thp5016 <thp5016@psu.edu>
2013-04-12 16:17:04 -0300

    Session manager changes navbar when user logs in

--- workspace/My Open Journal/src/Test/SessionManager.java
+++ workspace/My Open Journal/src/Test/SessionManager.java
@@ -10,29 +10,68 @@
 
 package Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.Page;
 import org.zkoss.zk.ui.Session;
 import org.zkoss.zk.ui.Sessions;
-import org.zkoss.zul.Window;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Initiator;
+import org.zkoss.zul.Hbox;
+import org.zkoss.zul.Include;
 
 // This class provides Cookie functionality for the website so that users can login
 // and logout, and so that the website can provide user-specific functionality
-public class SessionManager extends Window
-{
-  public void setSession(String username, String password)
-  {
-    try{
-      Session s = Sessions.getCurrent();
-      s.setAttribute("user", username);
-      Executions.sendRedirect("index.zul");
-    }finally {}
-  }	
-  public void checkSession() 
-  {
-	  boolean valid = Sessions.getCurrent().getAttribute("user") != null ? true : false;
-	  if(!valid)
-	  {
-		  Executions.sendRedirect("timeout.zul");
-	  }
-  }
+public class SessionManager extends SelectorComposer<Component> implements Initiator {
+
+	@Wire("#loggedIn")
+	Hbox loggedIn;
+	
+	@Wire("#notLoggedIn")
+	Hbox notLoggedIn;
+	
+	// Creates a new session that means that the user has logged in
+	public static void setSession(String username, String password) {
+		try {
+			Sessions.getCurrent().setAttribute("user", username);
+			Sessions.getCurrent().setAttribute("password", password);
+			Executions.sendRedirect("index.zul");
+		} 
+		finally {
+		}
+	}
+
+	// Checks to see if the user is logged in
+	public boolean checkSession() {
+		Session zkSession = Executions.getCurrent().getDesktop().getSession();
+		String username = (String) zkSession.getAttribute("user");
+		if (username == null) {
+			return false;
+		} 
+		else {
+			return true;
+		}
+	}
+
+	// Checks to see if user is logged in and shows the correct navbar
+	@Override
+	public void doInit(Page arg0, Map<String, Object> arg1) throws Exception {
+		Session zkSession = Executions.getCurrent().getDesktop().getSession();
+		String username = (String) zkSession.getAttribute("user");
+		if (username == null) {
+	        final HashMap<String, Object> map = new HashMap<String, Object>();
+	        Executions.createComponents("navbar.zul", null, map);
+			return;
+		} 
+		else {
+	        final HashMap<String, Object> map = new HashMap<String, Object>();
+	        Executions.createComponents("navbar2.zul", null, map);
+			return;
+		}
+
+	}
 }
