9fb91174cd2dd2e0b3bf3664c217a2ad6f9d7f84
thp5016 <thp5016@psu.edu>
2013-04-16 12:03:34 -0400

    Changed homepage columns

--- workspace/My Open Journal/src/Test/DBManager.java
+++ workspace/My Open Journal/src/Test/DBManager.java
@@ -4,9 +4,6 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JOptionPane;
-
-
 
 public class DBManager {
 
@@ -111,6 +108,32 @@ public class DBManager {
 		return null;
 	}
 		
+	public List<Data> GetNewPapers()
+	{
+		String query;
+		ResultSet rs;
+		connection = new DBConnection("10.2.65.20", "myopenjournal", "sa", "umaxistheman");
+    	query = "select top 10 * from Papers order by Upload_Date desc;";
+		try {
+			PreparedStatement stmt = connection.GetConnection().prepareStatement(query);
+			rs = stmt.executeQuery();
+	    	List<Data> rowValues = new ArrayList<Data>();
+
+	    	while (rs.next()) {
+	    		Data data = new Data(rs.getString(3), rs.getString(8), rs.getString(9));
+	    	    rowValues.add(data);
+	    	}
+
+	    	rs.close();
+			stmt.close();
+	    	connection.Disconnect();
+	    	return rowValues;
+		} 
+		catch (SQLException e) {
+			System.out.println("Failure to Get Top Papers: " + e.getMessage());
+		}
+		return null;
+	}
 	
 	public boolean IsValidPassword(String user, String pass){
 		String query;
