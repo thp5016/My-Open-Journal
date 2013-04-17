8c1e15ba6ad230094acf4c56179847311516d074
thp5016 <thp5016@psu.edu>
2013-04-17 16:14:33 -0400

    fixed upload form

--- workspace/My Open Journal/src/Test/AddReview.java
+++ workspace/My Open Journal/src/Test/AddReview.java
@@ -4,6 +4,7 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Textbox;
