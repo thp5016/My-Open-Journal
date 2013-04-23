8e74e72ddd5812ef2c0137966eb8f224c9c17bcc
EricDundore <esd5065@psu.edu>
2013-04-23 15:03:26 -0300

    manager.getID(paper)

--- workspace/My Open Journal/src/Test/Paper.java
+++ workspace/My Open Journal/src/Test/Paper.java
@@ -10,37 +10,38 @@ import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Textbox;
 import org.zkoss.zul.impl.InputElement;
 
-public class Paper extends GenericForwardComposer {
+public class Paper extends GenericForwardComposer 
+{
+  //need to implement a funciton in session manager
+  int paper_id = manager.GetID(paper);
 
-//need to implement a funciton in session manager
-int paper_id = manager.GetID(paper);
-
-@Listen("onClick = #downloadLink")
-public void viewPaper()
-{
-  // redirect to the pdf id specified by the page url
-  // 
-}
-@Listen("onClick = #upVotes")
-public void upVote()
-{
-  // add a new entry to the paper votes table
-  // update the paper's upvotes count
-}
-@Listen("onClick = #downVotes")
-public void downVote()
-{
-  // add a new entry to the papers votes table
-  // update the paper's downvotes count
-}
-@Listen("onClick = #reviewLink")
-public void viewReview()
-{
-  // redirect to reviews page
-}
-@Listen("onClick = #addReviewLink")
-public void addReview()
-{
-  // redirect to add review page
-}
+  @Listen("onClick = #downloadLink")
+  public void viewPaper()
+  {
+    // redirect to the pdf id specified by the page url
+    // Executions.sendRedirect(/* pdf url */);
+  }
+  @Listen("onClick = #upVotes")
+  public void upVote()
+  {
+    // add a new entry to the paper votes table
+    // update the paper's upvotes count
+    
+  }
+  @Listen("onClick = #downVotes")
+  public void downVote()
+  {
+    // add a new entry to the papers votes table
+    // update the paper's downvotes count
+  }
+  @Listen("onClick = #reviewLink")
+  public void viewReview()
+  {
+    // redirect to reviews page
+  }
+  @Listen("onClick = #addReviewLink")
+  public void addReview()
+  {
+    // redirect to add review page
+  }
 }
